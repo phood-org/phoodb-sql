5d4db0dd48aff5927df4041f55d24e1113eab55c
Pearse Hutson <pearse.2008@gmail.com>
2017-08-06 11:29:34 -0500

    Changed database type to Azure SQL Database V12

--- phoodb-sql/userUten.sql
+++ phoodb-sql/userUten.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[userUten]
 (
-	[UserId] INT NOT NULL PRIMARY KEY
+	[UserId] INT NOT NULL, 
+    [UtenId] INT NOT NULL,
+	PRIMARY KEY([UserId], [UtenId]),
+	FOREIGN KEY ([UserId]) REFERENCES [dbo].[enduser](ID) ,
+	FOREIGN KEY ([UtenId]) REFERENCES [dbo].[utensil](ID) 
 )
